0b4019f9296020264a27f26e459e0bd5510880b3
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-05-09 13:56:34 -0400

    Add SPA pages & Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Rockets from './pages/rockets';
+import Missions from './pages/missions';
+import MyProfile from './pages/myProfile';
 import NavBar from './components/NavBar';
 
 export default function App() {
   return (
     <div>
       <NavBar />
-      App
+      <Routes>
+        <Route exact path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/my_profile" element={<MyProfile />} />
+      </Routes>
     </div>
   );
 }
