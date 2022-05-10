aed31e6294463fe02517f23447d545bcb14578e4
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-05-10 16:41:54 -0400

    Add Rockets to rocket page

--- src/pages/rockets.js
+++ src/pages/rockets.js
@@ -1,11 +1,25 @@
 import { useSelector } from 'react-redux';
+import RocketCard from '../components/rocketCard';
 
 const Rockets = () => {
   const rocketData = useSelector((state) => state.rockets.value);
-  rocketData.forEach((element) => {
-    console.log(element.id);
-  });
-  return <div>Rockets</div>;
+
+  return (
+    <div className="rocketContainer">
+      <ul>
+        {rocketData.map((rocket) => (
+          <li key={rocket.id}>
+            <RocketCard
+              imgSource={rocket.flickr_images[0]}
+              rocketName={rocket.rocket_name}
+              rocketDescription={rocket.description}
+              reserved={Boolean(false)}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Rockets;
