3000daf2214a172054c9763950bcdceb0599624b
Aaron Keegan <AKeegan.Dev@gmail.com>
2022-05-10 21:05:40 -0400

    setup reservation store logic

--- src/components/rocketCard.js
+++ src/components/rocketCard.js
@@ -1,50 +1,72 @@
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 import { useState } from 'react';
+import { changeRocketReservation } from '../redux/rockets/rockets';
 import '../styles/rocketCard.css';
 
 const RocketCard = (props) => {
-  const {
-    imgSource, rocketName, rocketDescription, reserved,
-  } = props;
+  const dispatch = useDispatch();
 
-  const [isActive, setActive] = useState(reserved);
-  const [classNames, setClassNames] = useState('reserveButton notReserved');
-  const [buttonText, setButtonText] = useState('Reserve Rocket');
+  const {
+    imgSource, rocketName, rocketDescription, reserved, rocketID,
+  } = props;
 
   const classNameKey = {
     reserved: {
       classNames: 'reserveButton reserved',
-      buttonText: 'Reserve Rocket',
+      buttonText: 'Cancel Reservation',
+      badge: 'badge visible',
     },
     notReserved: {
       classNames: 'reserveButton notReserved',
-      buttonText: 'Cancel Reservation',
+      buttonText: 'Reserve Rocket',
+      badge: 'badge',
     },
   };
 
-  const setReserved = () => {
+  const [isActive, setActive] = useState(reserved);
+  const [classNames, setClassNames] = useState(
+    reserved ? classNameKey.reserved.classNames : classNameKey.notReserved.classNames,
+  );
+  const [buttonText, setButtonText] = useState(
+    reserved ? classNameKey.reserved.buttonText : classNameKey.notReserved.buttonText,
+  );
+  const [spanVisibility, setSpanVisibility] = useState(
+    reserved ? classNameKey.reserved.badge : classNameKey.notReserved.badge,
+  );
+
+  const setReserved = (id) => {
+    setActive(!isActive);
     if (classNames === classNameKey.reserved.classNames) {
       setClassNames(classNameKey.notReserved.classNames);
       setButtonText('Reserve Rocket');
+      setSpanVisibility(classNameKey.notReserved.badge);
     } else {
       setClassNames(classNameKey.reserved.classNames);
       setButtonText('Cancel Reservation');
+      setSpanVisibility(classNameKey.reserved.badge);
     }
-
-    setActive(!isActive);
+    dispatch(changeRocketReservation(id));
   };
+  // displays the reserved badge above the rocket description text
+  const Badge = () => (
+    <span className={spanVisibility}>Reserved</span>
+  );
 
   return (
     <div className="rocketCard">
       <img className="rocketImage" src={imgSource} alt="" />
       <div className="rocketDetailsContainer">
         <span className="rocketTitle">{rocketName}</span>
-        <p className="rocketDetails">{rocketDescription}</p>
+        <p className="rocketDetails">
+          <Badge />
+          {rocketDescription}
+        </p>
         <button
           type="button"
           className={classNames}
           onClick={() => {
-            setReserved();
+            setReserved(rocketID);
           }}
         >
           {buttonText}
@@ -55,6 +77,7 @@ const RocketCard = (props) => {
 };
 
 RocketCard.propTypes = {
+  rocketID: PropTypes.number.isRequired,
   imgSource: PropTypes.string.isRequired,
   rocketName: PropTypes.string.isRequired,
   rocketDescription: PropTypes.string.isRequired,
--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -11,12 +11,27 @@ const rocketsSlice = createSlice({
   initialState: {
     value: [],
   },
-  reducers: {},
+  reducers: {
+    changeRocketReservation: (state, action) => {
+      console.log('I have been triggered');
+      const index = state.value.findIndex((rocket) => rocket.id === action.payload);
+      console.log(index);
+      state.value[index].reserved = !state.value[index].reserved;
+    },
+  },
   extraReducers: {
     [getRocketData.fulfilled]: (state, action) => {
-      state.value = action.payload;
+      state.value = action.payload.map((rocket) => ({
+        id: rocket.id,
+        flickr_images: rocket.flickr_images[0],
+        rocket_name: rocket.rocket_name,
+        description: rocket.description,
+        reserved: Boolean(false),
+      }));
     },
   },
 });
+
+export const { changeRocketReservation } = rocketsSlice.actions;
 
 export default rocketsSlice.reducer;
