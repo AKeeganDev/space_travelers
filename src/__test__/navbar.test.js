41c86af31a4864baeb8e8972a1d611956bef2570
fasaldana <fernando_s_98@hotmail.com>
2022-05-13 11:37:57 -0500

    Testing for missions and navbar

--- src/__test__/navbar.test.js
+++ src/__test__/navbar.test.js
@@ -1,6 +1,6 @@
 import { BrowserRouter } from 'react-router-dom';
 import TestRenderer from 'react-test-renderer';
-import Navbar from '../components/Navbar';
+import Navbar from '../components/NavBar';
 
 it('Check if Navbar renders correctly', () => {
   const tree = TestRenderer.create(
